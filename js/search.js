4ea3db0b070d095cc4c8b6346198f41f212e46ed
Albert <albertkr@wp.pl>
2022-05-18 20:14:45 +0200

    Added shuffling games

--- js/search.js
+++ js/search.js
@@ -2,9 +2,14 @@ import { makeTile } from "./gameTile.js"
 
 export const selectOptions = (resLink) =>
 {
-    const existingTile = document.querySelectorAll('.game-tile')
+    let existingTile = document.querySelectorAll('.game-tile-dark')
     const existingModalOverlays = document.querySelectorAll('.modal-overlay')
-    const existingModals = document.querySelectorAll('.modal-container')
+    let existingModals = document.querySelectorAll('.modal-container-dark')
+    if(localStorage.getItem('color') === 'white')
+    {
+        existingModals = document.querySelectorAll('.modal-container-light')
+        existingTile = document.querySelectorAll('.game-tile-light')
+    }
     existingTile.forEach(tile => tile.remove())
     existingModalOverlays.forEach(tile => tile.remove())
     existingModals.forEach(tile => tile.remove())
@@ -13,7 +18,11 @@ export const selectOptions = (resLink) =>
 
 export const search = (resLink) =>
 {
-    const existingTile = document.querySelectorAll('.game-tile')
+    let existingTile = document.querySelectorAll('.game-tile-dark')
+    if(localStorage.getItem('color') === 'white')
+    {
+        existingTile = document.querySelectorAll('.game-tile-light')
+    }
     existingTile.forEach(tile => tile.remove())
     makeTile(resLink)
 }
